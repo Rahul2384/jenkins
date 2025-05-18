e60f69dd74f76dc48fdea79e7480425393875d13
Rahul Kasula <rahulkasula23@gmail.com>
2025-05-18 22:22:40 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,9 @@
 def div():
     print("this is division method")
-    return 3 / 1  # Changed denominator from 0 to 1 to avoid ZeroDivisionError
+    return 3 / 0
 
 def check():
-    return "h"  # Added missing closing quotation mark
+    return "h
 
 if __name__ == "__main__": 
     div()
